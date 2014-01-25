3e01613c37d11598a704cfe4e80ac2247b2e4030
zenyagami <zenyagami@gmail.com>
2014-01-25 01:13:24 -0600

    Added ParseInfo

--- src/com/hackdf/autochilango/entities/Car.java
+++ src/com/hackdf/autochilango/entities/Car.java
@@ -1,25 +1,47 @@
 package com.hackdf.autochilango.entities;
 
+import java.util.ArrayList;
+
 public class Car {
 	//placas
 	private String plates;
 	//debe tenencia
-	private boolean hasDebts;
+	private int hasDebts;
+	private ArrayList<Offenses> offenseList;
+	private ArrayList<Verificacion> VerificationList;
+	public ArrayList<Offenses> getOffenseList() {
+		return offenseList;
+	}
+	public void setOffenseList(ArrayList<Offenses> offenseList) {
+		this.offenseList = offenseList;
+	}
+	public ArrayList<Verificacion> getVerificationList() {
+		return VerificationList;
+	}
+	public void setVerificationList(ArrayList<Verificacion> verificationList) {
+		VerificationList = verificationList;
+	}
+	public int getHasDebts() {
+		return hasDebts;
+	}
 	public String getPlates() {
 		return plates;
 	}
 	public void setPlates(String plates) {
 		this.plates = plates;
 	}
-	public boolean isHasDebts() {
+	public int isHasDebts() {
 		return hasDebts;
 	}
-	public void setHasDebts(boolean hasDebts) {
+	public void setHasDebts(int hasDebts) {
 		this.hasDebts = hasDebts;
 	}
-	public Car(String plates, boolean hasDebts) {
+	public Car(String plates, int hasDebts) {
 		super();
 		this.plates = plates;
 		this.hasDebts = hasDebts;
+	}
+	public Car() {
+		// TODO Auto-generated constructor stub
 	} 
 }
---
+++ src/com/hackdf/autochilango/utils/Parser.java
@@ -0,0 +1,35 @@
+package com.hackdf.autochilango.utils;
+
+import org.json.JSONObject;
+
+import com.hackdf.autochilango.entities.Car;
+
+public class Parser {
+	public static Car ParseCarInfoFromJson(JSONObject response)
+	{
+		if(response==null || !response.has("consulta"))
+		{
+			return null;
+		}
+		Car carInfo = new Car();
+		try {
+			carInfo.setPlates(response.getString("placa"));
+			if(response.has("tenencias") && response.getJSONObject("tenencias").has("tieneadeudos"))
+			{
+				carInfo.setHasDebts(Integer.valueOf(response.getJSONObject("tenencias").getString("tieneadeudos")));
+				
+			}
+			
+			
+		} catch (Exception e) {
+		}
+		
+		
+		
+		
+		
+		return carInfo;
+		
+	}
+
+}
