fead732bcd92bcfdac0f85f0ac725ebe9ba6b9b6
zenyagami <zenyagami@gmail.com>
2014-01-25 19:00:09 -0600

    UI fix

--- src/com/hackdf/autochilango/utils/Utils.java
+++ src/com/hackdf/autochilango/utils/Utils.java
@@ -6,12 +6,34 @@ import android.net.NetworkInfo;
 
 public class Utils {
 	public static boolean isOnline(Context context) {
-	    ConnectivityManager cm =
-	        (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-	    NetworkInfo netInfo = cm.getActiveNetworkInfo();
-	    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-	        return true;
-	    }
-	    return false;
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
 	}
+
+	public static String convertFromUTF8(String s) {
+		String out = null;
+		try {
+			out = new String(s.getBytes("ISO-8859-1"), "UTF-8");
+		} catch (java.io.UnsupportedEncodingException e) {
+			return null;
+		}
+		return out;
+	}
+
+	// convert from internal Java String format -> UTF-8
+	public static String convertToUTF8(String s) {
+		String out = null;
+		try {
+			out = new String(s.getBytes("UTF-8"), "ISO-8859-1");
+		} catch (java.io.UnsupportedEncodingException e) {
+			return s;
+		}
+		return out;
+	}
+
 }
